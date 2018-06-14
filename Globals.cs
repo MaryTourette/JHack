f2f6b34adcffb9f6747be349e189180a2ef328b4
MaryTourette <40146017+MaryTourette@users.noreply.github.com>
2018-06-14 23:49:18 +0200

    Add files via upload
    added logging

---
+++ Globals.cs
@@ -0,0 +1,14 @@
+﻿/**
+ * 
+ * This class store details about a logged in user
+ * 
+ **/
+
+namespace filebrowser
+{
+    //class who save logged in users
+    class Globals
+    {
+        public static User LoggedInUser { get; set; }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿/**
+ * 
+ * This class store user details including the JASON Web Token
+ * 
+ **/
+
+namespace filebrowser
+{
+    //Class of users with details and jwt token
+    class User
+    {
+        public string Username { get; set; }
+        public string eMail { get; set; }
+        public string access_token { get; set; }
+    }
+}
